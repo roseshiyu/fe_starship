f153000d6c223c262c0a93ced14b3b884427c00d
roseshiyu <roseshiyu@gmail.com>
2024-02-23 18:21:02 +0800

    install package reference nuxt dashboard

--- app.vue
+++ app.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#111827' : 'white')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Starship - Your Passport to Infinite Knowledge'
+const description = 'Starship is a cool learning tool that helps you discover new things in a fun way. It\'s like a spaceship for your brain, taking you on exciting journeys to learn and explore. With Starship, you can travel through the universe of knowledge and make learning an awesome adventure!.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://dashboard-template.nuxt.dev/social-card.png',
+  twitterImage: 'https://dashboard-template.nuxt.dev/social-card.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
